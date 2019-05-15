d944015d2f8f28912e952854579f2abf36b25164
zhangsan <zhangsan@qq.com>
2019-05-15 09:56:35 +0800

    本次修改添加了Student实体类

---
+++ src/cn/kgc/tangcco/zhy/entity/Student.java
@@ -0,0 +1,18 @@
+package cn.kgc.tangcco.zhy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 学生实体类
+ * @author zhy
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+   private String stuName;
+   private int stuNo;
+}
